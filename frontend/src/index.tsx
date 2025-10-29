c42860ecc737e4c0427749a6c9c32e63885a70c3
redjguilaran06 <redjguilaran06@gmail.com>
2025-10-29 10:10:33 +0800

    feat:added the expense design and fixed the income design

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing/Landing';
-import Signup from './pages/Signup/Signup';
-import Login from './pages/Login/login';
 import Dashboard from './pages/Dashboard/Dashboard';
-
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
     <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <Dashboard />
     </React.StrictMode>
   );
 }
